663dd7bcb43d7d9b18047a9ad2dc751921c10952
dapie346 <dawid.pietruszewski@gmail.com>
2022-08-24 13:17:37 +0200

    imported libraries, added logic

--- generator.py
+++ generator.py
@@ -1,1 +1,17 @@
-player = 0
+import random
+import string
+
+print("Welcome to the password generator")
+password_length = int(input("Enter password length: "))
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+#random.shuffle(characters)
+
+password = []
+for i in range(password_length):
+	password.append(random.choice(characters))
+
+print("".join(password))
+
+
+
